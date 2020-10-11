7275c8c4e09dcaf2cc6f30da185a838405f86e99
Akira Fushimi <tanafushi@gmail.com>
2020-10-11 07:41:44 +0000

    remove extra comment and code

--- shell/script/square-root.sh
+++ shell/script/square-root.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# 平方根の近似値の計算
-
-number=$1 #対象となる数
+number=$1
 decimalPlace=$2 #小数第何位まで計算するか
 ans=0
 end=0 # ちょうど一致する値が出たとき,ネストしたループを抜けるために使用
@@ -21,7 +19,7 @@ if [ `echo "scale=$decimalPlace; $number < 0" | bc` == 1 ]; then
 fi
 
 if [[ $number =~ [^[:digit:]] ]] || [[ $decimalPlace =~ [^[:digit:]] ]] ; then
-# shellでは/dや\wが使えない為、POSIX表記で正規表現を記述した。
+# shellでは/dや/wが使えない為、POSIX表記で正規表現を記述した。
   echo "引数が数値でない > $number , $decimalPlace"
   exit
 fi
@@ -30,35 +28,29 @@ if [ $number == 0 ]; then
   echo $number
   exit
 fi
-# 計算
+
+# 求める小数の数だけループをまわす
 for count in `seq 0 $decimalPlace`
 do
   for i in `seq 1 100`
   do
-    if [ $count != 0 ]; then
-      i=`echo "scale=$decimalPlace; $ans + $i * 0.1 ^ $count" | bc`
-    fi
+     # $iを$countが増えるにしたがって1倍、0.1倍、0.01倍...と小さくする。
+     i=`echo "scale=$decimalPlace; $ans + $i * 0.1 ^ $count" | bc`
+
     square=`echo "scale=$decimalPlace; $i ^ 2" | bc`
-    #echo $square
+    # 完全に一致した値
     if [ `echo "scale=$decimalPlace; $square == $number" | bc` == 1 ]; then
       ans=$i
       end=1
       break
+    # 一致しないため次のループへ
     elif [ `echo "scale=$decimalPlace; $square > $number" | bc` == 1 ]; then
-      if [ $count == 0 ]; then
-        #echo "break1 : $square"
-        ans=$((i - 1))
-        break
-      else
-        #echo "break2"
-        ans=`echo "scale=$decimalPlace; $previousNumber - 0.1 ^ $count" | bc`
-        break
-      fi 
+      ans=$previousNumber
+      break
     fi
      previousNumber=$i
   done
   if [ $end == 1 ]; then
-    #echo "break4"
     break
   fi
 done
