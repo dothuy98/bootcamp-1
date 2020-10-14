b39b68f465bf4a27766a1e2ca8d8040d6f625adc
Akira Fushimi <tanafushi@gmail.com>
2020-10-14 14:37:58 +0000

    add function about check-parameter

---
+++ shell/script/check-parameter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function checkParameter () {
+  if [ $1 == 0 ]; then
+    echo "引数を設定してください" >&2
+    exit 1
+  fi
+   
+  if [ $1 -gt 1 ]; then
+    echo "warning : 引数が2つ以上です。１つ目の引数のみを使用します。"
+  fi
+}
