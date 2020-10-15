261a52a4b3b0b3adcf5bd1e96f5ad0fda6c613eb
Akira Fushimi <tanafushi@gmail.com>
2020-10-15 03:33:28 +0000

    modify camel case to snake case

--- shell/script/test_check-relative-or-absolute-path.sh
+++ shell/script/test_check-relative-or-absolute-path.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-runLs=`ls`
-files=(${runLs})
-runPwd=`pwd`
-testParameters=("${files[0]}" "$runPwd" "xxx" "../" "~" "-" "aa bb")
-for parameter in "${testParameters[@]}"
+run_ls=`ls`
+files=(${run_ls})
+run_pwd=`pwd`
+test_parameters=("${files[0]}" "$run_pwd" "xxx" "../" "~" "-" "aa bb")
+for parameter in "${test_parameters[@]}"
 do
   echo "引数 : $parameter >"
   sh check-relative-or-absolute-path.sh $parameter
