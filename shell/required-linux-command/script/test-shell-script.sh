cc1a5c00f3b3b9459a67c8924820e746748e85b9
Akira Fushimi <tanafushi@gmail.com>
2020-10-21 03:44:00 +0000

    modify script and add new file

--- shell/required-linux-command/script/test-shell-script.sh
+++ shell/required-linux-command/script/test-shell-script.sh
@@ -1,45 +1,27 @@
 #!/bin/bash
 
 # 引数て指定したshell scriptをテストするスクリプトを必修コマンドを組み合わせて作成しました。
-# sh test-shell-script シェルスクリプト テストファイル [-p <number>]
 # テストファイルについて：1行に1度テストを行い、複数の引数はスペース区切りで指定してください。
-# -pでバックグラウンドで実行する個数を指定できます。
 
 # 例外処理
-if [ $# == 0 ]; then
-  echo "引数を指定してください"
-  echo "スレッドの指定は-p <number>でお願いします"
-  echo "メモリー・スワップの状況を監視するインターバルの指定は-i <number>でお願いします"
+if [ $# != 2 ]; then
+  echo "引数を2つ指定してください"
   exit 1
 fi
-  
-# 引数処理
 
 # 初期値
 interval=1
-threads=3
-
-count=$#
-for ((index=1; index<=$count; index++))
-do
-  next_index=$(($index + 1))
-  # -pオプションが存在した場合、次の引数を変数に格納
-  echo "${!index}" | grep -q -- "-p" && threads=${!next_index} && index=$(($index + 1)) && continue
-  echo "${!index}" | grep -q -- "-i" && interval=${!next_index} && index=$(($index + 1)) && continue
-  echo "${!index}" | grep -q -- "\.sh" && shell_script="${!index}" && continue
-  test_file="${!index}"
-done
-
-if [ -z $shell_script ]; then
+if `echo "${1}" | grep -q -- "\.sh"`;  then
+  shell_script=$1
+  test_file=$2
+elif `echo "${2}" | grep -q -- "\.sh"`; then
+  shell_script=$2
+  test_file=$1
+else
   echo "シェルスクリプトを拡張子を含めて正しく指定してください。"
   exit 1
 fi
-
-echo "testfile : $test_file"
-echo "shellscript : $shell_script"
-echo "interval : $interval"
-echo -e "threads : $threads\n"
-
+  
 script_path=`find . $shell_script`
 if [ $? != 0 ]; then
   echo "引数に指定されたスクリプトが見つかりません"
@@ -62,37 +44,18 @@ while_pid=$!
 trap 'kill $tail_pid $while_pid' Exit
 
 # テスト実行
-index=1
-end_file=`cat $test_file | wc -l`
+ls test-output.txt > /dev/null 2>&1 rm test-output.txt
 while read line
 do
-  {
-  echo "引数:$line" >> tmp$index.txt 2>&1
-  echo "$line" | xargs sh ${shell_script} >> tmp$index.txt 2>&1
+  echo "引数:$line" >> test-output.txt
+  echo "$line" | xargs sh ${shell_script} >> test-output.txt
   if [ $? != 0 ]; then 
-    echo "正常に終了しませんでした" >> tmp$index.txt
+    echo "正常に終了しませんでした" >> test-output.txt
   fi
   # 改行出力
-  echo >> tmp$index.txt
-  } &
-  pids+=($!)
-  if [ $index == $threads -o $index == "$end_file" ]; then
-    wait ${pids[@]}
-    index=0
-    pids=()
-  fi
-  index=$(($index+1))
-# done < $test_file
-done < test
+  echo >> test-output.txt
+done < $test_file
 
-# 分割ファイルを結合
-# 初期化
-ls output.txt > /dev/null 2>&1 && rm output.txt
-for index in `seq 1 $threads`
-do
-  cat "tmp${index}.txt" >> output.txt
-done
+cat test-output.txt
 
-rm tmp*
-echo "出力結果をoutput.txtに格納しました"
-#exit 0
+
