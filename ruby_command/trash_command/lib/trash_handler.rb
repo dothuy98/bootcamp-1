6462ecb06158d95ceb606e5cb09ddc3ef688bef1
Fushimi Akira <akira147369@outlook.jp>
2020-10-29 15:14:11 +0000

    modify code

--- ruby_command/trash_command/lib/trash_handler.rb
+++ ruby_command/trash_command/lib/trash_handler.rb
@@ -1,100 +1,89 @@
-require './lib/csv_editor'
+require './lib/csv_data'
 require './lib/trashed_file'
 require 'fileutils'
 
 class TrashHandler
+  
   TRASH_PATH = "#{Dir.home}/.trash"
-  CSV_PATH = "#{TRASH_PATH}/trashed_info.csv"
-  HEADER = ['file_name', 'original_path','compressed?']
-  HOW_TO_USE = <<~USAGE
-Usage: trash_handler [options] ARGV
-    -r, --restore VALUE              restore file to original position from ~/.trash 
-    -v, --view                       view file in .trash
-    -c, --compress                   compress file or directory by zip format
-    -d, --delete VALUE               delete file or directory in ~/.trash 
-    --delete-all                     delete all file and directory in ~/.trash
-USAGE
-
-  attr_reader :argvs, :editor 
+  CSV_PATH = "#{TRASH_PATH}/trashed_log.csv"
+  HEADER = ['file_name', 'original_path','compressed']
   
-  attr_accessor :trashed
+  attr_accessor :trashed_file, :method, :log
   
-  def initialize(argvs)
-    puts HOW_TO_USE if argvs.empty?
-    @csv = CsvEditor.new(CSV_PATH)
+  def initialize(options: [])
     build_settings
-    @argvss = argvs
+    apply_options(options)
   end
   
-  def run
-    @argvs.each_with_index do |value, index| 
-      dump(value, find_compress) if index < last_index = @argvs.find_index { |index| /^-/.match(index) } || @argvs.size
-      next if /^[^-]/.match(value)
-      case value
-      when "-r","--restore"
-        restore(check_exist(@argvs[index +  1]))
-      when "-v","--view"
-        view
-      when "-d", "--delete"
-        delete(check_exist(@argvs[index + 1]))
-      when "--delete-all"
-        delete_all
-      when "-c","--compress"
-      else
-        raise "\nerror: unknown predicate `#{value}'"
-      end
-    end
+  def run(file_names: [])
+    file_names.each { |file_name| dump(file_name) }
   end
   
   private
   
-  def build_settings
-    Dir.mkdir(TRASH_PATH) unless Dir.exist?(TRASH_PATH)
-    @csv.write_newly(HEADER) unless File.exist?(CSV_PATH)
+  def apply_options(options)
+    options.each_with_index do |value, index| 
+      next if /^[^-]/.match(value)
+      case value
+      when "-r","--restore"
+        restore(check_exist(options[index +  1]))
+      when "-v","--view"
+        view
+      when "-d", "--delete"
+        delete(check_exist(options[index + 1]))
+      when "--delete-all"
+        delete_all
+      when "-c","--compress"
+        @method = options[index + 1] || 'zip'
+      else
+        raise "error: unknown predicate `#{value}'"
+      end
+    end
+    
   end
   
-  def find_compress
-    return nil if @argvs.find_index { |value| value == "-c" || value == "--compress" }.nil?
-    # 複数の圧縮形式に対応する場合
-    # @array[@array.find_index { |value| value == "-c" || value == "--compress" } + 1]
-    'zip'
+  def build_settings
+    @log = CsvData.new(CSV_PATH)
+    Dir.mkdir(TRASH_PATH) unless Dir.exist?(TRASH_PATH)
+    @log.create(HEADER) unless File.exist?(CSV_PATH)
   end
   
   def check_exist(value)
-    raise "\nerror: that element is not found" if value.nil?
-    raise "\nerror: that directory or file is not exist in ~/.trash" if @csv.count_samefile(value) == 0
+    raise "error: that options's argument is not found" if value.nil?
+    raise "error: that directory or file is not exist in ~/.trash" if @log.count_samefile(value) == 0
     value
   end
   
-  def dump(file, method)
-    raise "\nsame file is exist in ~/.trash. plese rename" unless @csv.count_samefile(file) == 0
-    raise "\nerror: no '#{file}' in current directory" unless File.exist?(file)
-    @trashed = TrashedFile.new(file)
-    @trashed.moveto(TRASH_PATH)
-    @csv.write([file, "#{Dir.pwd + '/' + file}", method.nil? ? false : method])
-    @trashed.compress(method) unless method.nil?
+  def dump(file_name)
+    raise "same file is exist in ~/.trash. plese rename" unless @log.count_samefile(file_name) == 0
+    raise "error: no '#{file_name}' in current directory" unless File.exist?(file_name)
+    trashed_file = TrashedFile.new(file_name, File.expand_path(file_name), @method)
+    trashed_file.move_to(TRASH_PATH)
+    @log.write(trashed_file.info)
+    trashed_file.compress unless @method.nil?
   end
   
-  def restore(file)
-    @trashed = TrashedFile.new("#{TRASH_PATH}/#{file}")
-    @trashed.uncompress(@csv.find_compress_method(file)) if @csv.find_compress_method(file)
-    @trashed.moveto(@csv.find_path(file))
-    @csv.write_2d_arr(@csv.select_other_than(file))
+  def restore(file_name)
+    trashed_file = TrashedFile.new("#{TRASH_PATH}/#{file_name}", nil, @log.find_matchline(file_name)['compressed'])
+    trashed_file.uncompress unless @log.find_matchline(file_name)['compressed'] == 'nil'
+    trashed_file.move_to(@log.find_matchline(file_name)['original_path'])
+    @log.update(@log.select_other_than(file_name), HEADER)
   end
   
   def view
-    return "~/.trash has not file or directory" if @csv.read.empty?
-    puts @csv.read.map { |array| array.join("\t") }
+    puts HEADER.join("\t")
+    @log.read.each { |texts| puts texts.values.join("\t") }
   end
   
-  def delete(file)
-    TrashedFile.new("#{TRASH_PATH}/#{file}").delete
-    @csv.write_2d_arr(@csv.select_other_than(file))
+  def delete(file_name)
+    TrashedFile.new("#{TRASH_PATH}/#{file_name}").delete
+    @log.update(@log.select_other_than(file_name), HEADER)
   end
   
   def delete_all
-    TrashedFile.new(TRASH_PATH).delete
+    FileUtils.rm_r(TRASH_PATH)
   end
+  
 end
 
 if __FILE__ == $0
--- ruby_command/trash_command/lib/trashed_file.rb
+++ ruby_command/trash_command/lib/trashed_file.rb
@@ -1,47 +1,35 @@
 class TrashedFile
+  
   TRASH_PATH = "#{Dir.home}/.trash"
   
-  attr_reader :file
+  attr_accessor :file_name, :file_path, :method
   
-  def initialize(file)
-    @file = file
+  def initialize(file_name, file_path = nil, method = nil)
+    @file_name = file_name
+    @file_path = file_path
+    @method = method
   end
   
-  def moveto(path)
-    raise unless system("mv #{@file} #{path}")
+  def move_to(file_path)
+    system("mv #{@file_name} #{file_path}")
   end
   
   def delete
-    FileUtils.rm_r(@file)
+    FileUtils.rm_r(@file_name)
   end
   
-  def delete_all
-    FileUtils.rm_r(@TRASH_PATH)
+  def compress
+    file_name = "#{TRASH_PATH}/#{@file_name}"
+    system("zip -r #{file_name + '.' + @method} #{file_name}")
   end
   
-  def compress(method)
-    file = "#{TRASH_PATH}/#{@file}"
-    case method
-    when "zip"
-      system("zip -r #{add_extension(file, method)} #{file}")
-    else
-      raise "does not support this compression method"
-    end
+  def uncompress
+    raise "error: unsupported extension" unless @method == "zip"
+    system("unzip #{@file_name + '.' + @method}")
   end
   
-  def uncompress(method)
-    case method
-    when "zip"
-      system("unzip #{add_extension(@file, method)}")
-    else
-      raise "\nerror: unsupported extension"
-    end
-  end
-  
-  private
-  
-  def add_extension(file, method)
-    "#{file}.#{method}"
+  def info
+    [@file_name, @file_path, @method.nil? ? 'nil' : @method]
   end
   
 end
