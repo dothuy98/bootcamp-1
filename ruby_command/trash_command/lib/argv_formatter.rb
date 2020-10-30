f8aa5c49384687fa8dc550a0cc98e60476fc05b5
Fushimi Akira <akira147369@outlook.jp>
2020-10-30 04:16:37 +0000

    modify code

--- ruby_command/trash_command/lib/argv_formatter.rb
+++ ruby_command/trash_command/lib/argv_formatter.rb
@@ -1,24 +1,12 @@
-require './lib/trash_handler'
-
 class ArgvFromatter
-  
-  HOW_TO_USE = <<~USAGE
-Usage: trash_handler [options] file_name
-    -r, --restore trashed_file        restore file to original position from ~/.trash 
-    -v, --view                        view file in .trash
-    -c, --compress [compress_method]  compress file or directory by zip format (default: zip)
-    -d, --delete trashed_file         delete file or directory in ~/.trash 
-    --delete-all                      delete all file and directory in ~/.trash
-USAGE
   
   attr_reader :orders
     
   def initialize(orders = [])
-    puts HOW_TO_USE if orders.empty?
     @orders = orders
   end
   
-  def file_names
+  def arguments
     return [] if /^-/.match(@orders.first)
     @orders[0...@orders.find_index { |index| /^-/.match(index) } || @orders.size]
   end
@@ -28,9 +16,4 @@ USAGE
     @orders[@orders.find_index { |index| /^-/.match(index) } || 0..]
   end
   
-end
-
-if __FILE__ == $0
-  trash = ArgvFromatter.new(ARGV)
-  TrashHandler.new(options: trash.options).run(file_names: trash.file_names)
 end
--- ruby_command/trash_command/lib/trash_handler.rb
+++ ruby_command/trash_command/lib/trash_handler.rb
@@ -1,30 +1,48 @@
 require './lib/csv_data'
 require './lib/trashed_file'
+require './lib/argv_formatter.rb'
 require 'fileutils'
 
 class TrashHandler
   
   TRASH_PATH = "#{Dir.home}/.trash"
   CSV_PATH = "#{TRASH_PATH}/trashed_log.csv"
-  HEADER = ['file_name', 'original_path','compressed']
+  HEADER = ['file_name', 'original_path', 'compressed']
+  HOW_TO_USE = <<~USAGE
+  Usage: trash_handler [options] file_name
+    -r, --restore trashed_file        restore file to original position from ~/.trash 
+    -v, --view                        view file in .trash
+    -c, --compress [compress_method]  compress file or directory by zip format (default: zip)
+    -d, --delete trashed_file         delete file or directory in ~/.trash 
+    --delete-all                      delete all file and directory in ~/.trash
+USAGE
   
-  attr_accessor :method, :log
+  attr_accessor :method, :log, :options
   
   def initialize(options: [])
+    @log = CsvData.new(CSV_PATH)
     build_settings
-    apply_options(options)
+    @options = options
+    @method = find_compress(options)
   end
   
   def run(file_names: [])
-    file_names.each { |file_name| dump(file_name) }
+    puts HOW_TO_USE if file_names.empty? && options.empty?
+    dump(file_names)
+    apply_options(@options)
   end
   
   private
   
+  def find_compress(options)
+    return nil unless options.any? { |option| /^-{1,2}c/.match(option) }
+    options[options.find_index { |option| /^-{1,2}c/.match(option) } + 1] || 'zip'
+  end
+  
   def apply_options(options)
-    options.each_with_index do |value, index| 
-      next if /^[^-]/.match(value)
-      case value
+    options.each_with_index do |option, index| 
+      next if /^[^-]/.match(option) || /^-{1,2}c/.match(option)
+      case option
       when "-r","--restore"
         restore(check_exist(options[index +  1]))
       when "-v","--view"
@@ -33,17 +51,13 @@ class TrashHandler
         delete(check_exist(options[index + 1]))
       when "--delete-all"
         delete_all
-      when "-c","--compress"
-        @method = options[index + 1] || 'zip'
       else
         raise "error: unknown predicate `#{value}'"
       end
     end
-    
   end
   
   def build_settings
-    @log = CsvData.new(CSV_PATH)
     Dir.mkdir(TRASH_PATH) unless Dir.exist?(TRASH_PATH)
     @log.create(HEADER) unless File.exist?(CSV_PATH)
   end
@@ -54,13 +68,15 @@ class TrashHandler
     value
   end
   
-  def dump(file_name)
-    raise "same file is exist in ~/.trash. plese rename" unless @log.count_samefile(file_name) == 0
-    raise "error: no '#{file_name}' in current directory" unless File.exist?(file_name)
-    trashed_file = TrashedFile.new(file_name, File.expand_path(file_name), @method)
-    trashed_file.move_to(TRASH_PATH)
-    @log.write(trashed_file.info)
-    trashed_file.compress unless @method.nil?
+  def dump(file_names)
+    file_names.each do |file_name| 
+      raise "same file is exist in ~/.trash. plese rename" unless @log.count_samefile(file_name) == 0
+      raise "error: no '#{file_name}' in current directory" unless File.exist?(file_name)
+      trashed_file = TrashedFile.new(file_name, File.expand_path(file_name), @method)
+      trashed_file.move_to(TRASH_PATH)
+      @log.write(trashed_file.info)
+      trashed_file.compress unless @method.nil?
+    end
   end
   
   def restore(file_name)
@@ -87,6 +103,8 @@ class TrashHandler
 end
 
 if __FILE__ == $0
-  TrashHandler.new(ARGV).run
+  argv = ArgvFromatter.new(ARGV)
+  TrashHandler.new(options: argv.options).run(file_names: argv.arguments)
 end
+
 
