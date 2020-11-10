8786682398023e0d0d440997bce5fdbd08feba7c
Fushimi Akira <akira147369@outlook.jp>
2020-11-10 06:00:44 +0000

    modify code

--- ruby_command/cli2/lib/file_filtable.rb
+++ ruby_command/cli2/lib/file_filtable.rb
@@ -1,9 +1,5 @@
 module FileFiltable
   
-  def include_file?(words)
-    words.any? { |word| File.file?(word) }
-  end
-  
   def extract_text(file_name)
     File.read(file_name).gsub("\n", '')
   end
